b024ea7a36b365c38ceffe1118550852ef96e9bc
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-13 21:18:28 +0530

    modified:   app/components/Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import burger from "../burger.jpg"
 
 export default function Header(){
     return (
         <div id="head">
-            <h2 className="text-xl">Jimmy's Diner</h2>
+            <h2 className="text-xl">Jimmy&apos;s Diner</h2>
             <p id="tagline">The best burgers and pizzas in town.</p>
         </div>
     )
